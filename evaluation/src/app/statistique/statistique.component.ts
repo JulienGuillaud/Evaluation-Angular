2d0d7ad143e4f7e3c5bf2416abcf12f405ce8cfb
JulienGuillaud <julien.guillaud00@gmail.com>
2021-06-29 12:21:39 +0200

    Exercice 4

--- evaluation/src/app/statistique/statistique.component.ts
+++ evaluation/src/app/statistique/statistique.component.ts
@@ -12,10 +12,15 @@ export class StatistiqueComponent implements OnInit {
   stat1 = new Statistique("zdh7465h1zdh5", "Test1", "valeur1", Appreciation.WARNING);
   stat2 = new Statistique("gk78uk9t871kt", "Test2", "valeur2", Appreciation.SUCCESS);
 
+  public tabStats = [
+    this.stat1, this.stat2
+  ];
 
+  
   constructor() { }
-
+  
   ngOnInit(): void {
+    setTimeout(() => {  this.tabStats.push(new Statistique("e89h74d58fgh6", "Test3", "valeur3", Appreciation.DANGER)) }, 3000);
   }
 
 }
